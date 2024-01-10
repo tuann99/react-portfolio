0ca8e35b7c94464528b09f6d8c9ba9b42df1f403
Tuan N. <tnguy256@jhu.edu>
2024-01-10 10:08:19 -0500

    modified:   components/experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -29,8 +29,8 @@ export default function Experience() {
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.5rem 2rem",
-                marginLeft: "1.5rem",
-                marginRight: "1.5rem",
+                marginLeft: "3rem",
+                marginRight: "3rem",
               }}
               contentArrowStyle={{
                 borderRight:
