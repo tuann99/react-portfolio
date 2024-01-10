3b63718a3ea821ee996d49375e65088217927d43
Tuan N. <tnguy256@jhu.edu>
2024-01-10 09:43:22 -0500

    modified:   components/intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Tuan.</span> I'm an{" "}
-        <span className="font-bold">aspiring software developer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Tuan.</span> I'm an aspiring software developer with{" "}
+        <span className="font-bold">2 years</span> of experience working in research labs.
       </motion.h1>
 
       <motion.div
